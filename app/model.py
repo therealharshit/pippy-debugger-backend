a1761cbcc7d8fa55fd0ce9534f92ed6977f38ebf
Harshit Verma <therealharshit014@gmail.com>
2025-08-12 19:27:33 +0530

    Add and use model gemma-12b-it and update model logic

--- app/model.py
+++ app/model.py
@@ -1,27 +1,35 @@
-from transformers import pipeline
+import os
+from dotenv import load_dotenv
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
 from app.prompts.prompts import CODE_DEBUG_PROMPT, CODE_CONTEXT_PROMPT
 
-model = pipeline("text-generation", model="Qwen/Qwen2.5-1.5B-Instruct", device=-1)
+load_dotenv()
+os.environ["GOOGLE_API_KEY"]=os.getenv("GOOGLE_API_KEY")
+## Langmith tracking
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+
+model = init_chat_model("gemma-3-12b-it", model_provider="google_genai")
 
 def debug_code(code: str):
-    prompt = CODE_DEBUG_PROMPT.format(code=code)
-    output = model(prompt, max_new_tokens=512, do_sample=True)
-
-    response = output[0]['generated_text']
-
-    if "### Response:" in response:
-        response = response.split("### Response:")[-1].strip()
-
+    prompt = ChatPromptTemplate.from_template(CODE_DEBUG_PROMPT)
+    
+    chain = prompt|model
+    output = chain.invoke({"code": code})
+    
+    response = output.content
+    
     return response
 
 
 def get_context(code: str):
-    prompt = CODE_CONTEXT_PROMPT.format(code=code)
-    output = model(prompt, max_new_tokens=512, do_sample=True)
+    prompt = ChatPromptTemplate.from_template(CODE_CONTEXT_PROMPT)
+    
+    chain = prompt|model
+    output = chain.invoke({"code": code})
 
-    response = output[0]['generated_text']
-
-    if "### Response:" in response:
-        response = response.split("### Response:")[-1].strip()
-
+    response = output.content
+    
     return response
